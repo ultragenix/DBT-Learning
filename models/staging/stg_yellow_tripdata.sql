098adeeabd02de2c55b57e7d3adbd05da4ecc801
ultragenix <mangaman.mouny@gmail.com>
2026-02-16 19:15:50 +0400

    add yallow taxi query

---
+++ models/staging/stg_yellow_tripdata.sql
@@ -0,0 +1,29 @@
+SELECT
+    -- identifiers
+    cast(VendorID as int) as vendor_id,
+    cast(RatecodeID as int) as ratecode_id,
+    cast(PULocationID as int) as pickup_location_id,
+    cast(DOLocationID as int) as dropoff_location_id,
+
+    -- timestamps
+    cast(tpep_pickup_datetime as timestamp) as tpep_pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as tpep_dropoff_datetime,
+
+    -- trip info
+    store_and_fwd_flag,
+    cast(passenger_count as int) as passenger_count,
+    cast(trip_distance as FLOAT64) as trip_distance,
+
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    cast(congestion_surcharge as numeric) as congestion_surcharge,
+    cast(airport_fee as numeric) as airport_fee,
+    cast(payment_type as numeric) as payment_type
+FROM {{source("raw_data", "green_tripdata")}}
+WHERE VendorID IS NOT NULL
